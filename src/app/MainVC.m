UInt2048/Spice

commit b15028eed123744d455e29ed7c32df1b7a475235

Update bundled substrate, preliminary Substitute work?

src/shared/jailbreak.m
@@ -407,12 +407,12 @@ kern_return_t jailbreak(uint32_t opt, void* controller, void (*sendLog)(void*, N
     }
 
     {
-        // check if substrate is not installed & install it from a deb file 
+        // Substrate is not in the bootstrap, we may need to install it
         if ((opt & JBOPT_POST_ONLY) == 0)
         {
-            if (access("/usr/libexec/substrate", F_OK) != 0)
+            if (access("/usr/libexec/substrate", F_OK) != 0 && access("/usr/lib/libsubstitute.dylib", F_OK) != 0)
             {
-                LOG("substrate was not found? installing it...");
+                LOG("substrate was not found. installing it...");
 
                 COPY_RESOURCE("mobilesubstrate.deb", "/jb/mobilesubstrate.deb");
 
@@ -439,34 +439,36 @@ kern_return_t jailbreak(uint32_t opt, void* controller, void (*sendLog)(void*, N
 
     {
         // handle substrate's unrestrict library 
+        if (access("/usr/libexec/substrate", F_OK) == 0)
+        {
+        	if (access("/Library/MobileSubstrate", F_OK) != 0)
+        	{
+            	mkdir("/Library/MobileSubstrate", 0755);
+        	}
+        	if (access("/Lbirary/MobileSubstrate/ServerPlugins", F_OK) != 0)
+        	{
+           		mkdir("/Library/MobileSubstrate/ServerPlugins", 0755);
+        	}
 
-        if (access("/Library/MobileSubstrate", F_OK) != 0)
-        {
-            mkdir("/Library/MobileSubstrate", 0755);
-        }
-        if (access("/Lbirary/MobileSubstrate/ServerPlugins", F_OK) != 0)
-        {
-            mkdir("/Library/MobileSubstrate/ServerPlugins", 0755);
-        }
+        	if ((opt & JBOPT_POST_ONLY) == 0)
+        	{
+            	if (access("/Library/MobileSubstrate/ServerPlugins/Unrestrict.dylib", F_OK) == 0)
+            	{
+                	unlink("/Library/MobileSubstrate/ServerPlugins/Unrestrict.dylib");
+                	LOG("deleted old Unrestrict.dylib");
+            	}
 
-        if ((opt & JBOPT_POST_ONLY) == 0)
-        {
-            if (access("/Library/MobileSubstrate/ServerPlugins/Unrestrict.dylib", F_OK) == 0)
-            {
-                unlink("/Library/MobileSubstrate/ServerPlugins/Unrestrict.dylib");
-                LOG("deleted old Unrestrict.dylib");
-            }
-
-            COPY_RESOURCE("Unrestrict.dylib", "/Library/MobileSubstrate/ServerPlugins/Unrestrict.dylib");
-            LOG("unrestrict: %d", access("/Library/MobileSubstrate/ServerPlugins/Unrestrict.dylib", F_OK));
-        }
-        else if (access("/Library/MobileSubstrate/ServerPlugins/Unrestrict.dylib", F_OK) != 0)
-        {
-            LOG("note: JBOPT_POST_ONLY mode but unrestrict.dylib was not found");
-        }
-        else
-        {
-            LOG("JBOPT_POST_ONLY mode and unrestrict is present, all is well");
+            	COPY_RESOURCE("Unrestrict.dylib", "/Library/MobileSubstrate/ServerPlugins/Unrestrict.dylib");
+            	LOG("unrestrict: %d", access("/Library/MobileSubstrate/ServerPlugins/Unrestrict.dylib", F_OK));
+        	}
+        	else if (access("/Library/MobileSubstrate/ServerPlugins/Unrestrict.dylib", F_OK) != 0)
+        	{
+            	LOG("note: JBOPT_POST_ONLY mode but unrestrict.dylib was not found");
+        	}
+        	else
+        	{
+            	LOG("JBOPT_POST_ONLY mode and unrestrict is present, all is well");
+        	}
         }
     }
 
@@ -508,30 +510,35 @@ kern_return_t jailbreak(uint32_t opt, void* controller, void (*sendLog)(void*, N
 	}
 
     {
-        if (access("/Library/Substrate", F_OK) == 0)
-        {
-            // move to old directory
-            NSString *newPath = [NSString stringWithFormat:@"/Library/Substrate.%lu", (unsigned long)time(NULL)];
-            LOG("moving /Library/Substrate to new path: %@", newPath);
+    	if (access("/usr/libexec/substrate", F_OK) == 0)
+    	{
+        	if (access("/Library/Substrate", F_OK) == 0)
+        	{
+            	// move to old directory
+            	NSString *newPath = [NSString stringWithFormat:@"/Library/Substrate.%lu", (unsigned long)time(NULL)];
+            	LOG("moving /Library/Substrate to new path: %@", newPath);
 
-            [fileMgr moveItemAtPath:@"/Library/Substrate" toPath:newPath error:nil];
+            	[fileMgr moveItemAtPath:@"/Library/Substrate" toPath:newPath error:nil];
 
-            if (access("/Library/Substrate", F_OK) == 0)
-            {
-                LOG("failed to move /Library/Substrate!!");
-                ret = KERN_FAILURE;
-                goto out;
-            }
-        }
+            	if (access("/Library/Substrate", F_OK) == 0)
+            	{
+                	LOG("failed to move /Library/Substrate!!");
+                	ret = KERN_FAILURE;
+                	goto out;
+            	}
+        	}
 
-        mkdir("/Library/Substrate", 1755);
+        	mkdir("/Library/Substrate", 1755);
 
-        if (access("/usr/libexec/substrate", F_OK) == 0)
-        {
             inject_trust("/usr/libexec/substrate");
 
             ret = execprog("/usr/libexec/substrate", NULL);
             LOG("substrate ret: %d", ret);
+        }
+        else if (access("/usr/lib/libsubstitute.dylib", F_OK) == 0)
+        {
+        	ret = execprog("/etc/rc.d/substitute-launcher", NULL);
+            LOG("substitute ret: %d", ret);
         }
         else if (opt & JBOPT_POST_ONLY)
         {

Makefile
@@ -68,6 +68,10 @@ $(APP)/bootstrap.tar.lzma:
 $(APP)/jailbreak-resources.deb:
 	echo Copying file to $@
 	cp $(RES)/jailbreak-resources.deb $@
+	
+$(APP)/mobilesubstrate.deb:
+	echo Copying file to $@
+	cp $(RES)/mobilesubstrate_0.9.7113_iphoneos-arm.deb $@
 
 # TODO: Make more accurate prerequisites
 
@@ -94,7 +98,7 @@ $(PAYLOAD): $(UNTETHER_SRC) $(SRC_ALL)/*.m $(SRC_ALL)/*.c $(SRC_CLI)/*.sh $(SRC_
 	mkdir -p $(SRC_CLI)/generated/package/DEBIAN && cp $(SRC_CLI)/control $(SRC_CLI)/generated/package/DEBIAN/control && cp $(SRC_CLI)/postinst $(SRC_CLI)/generated/package/DEBIAN/postinst
 	mkdir -p $(SRC_CLI)/generated/package/private/etc/racoon && cp $(SRC_CLI)/generated/install_stage1_2 $(SRC_CLI)/generated/package/private/etc/racoon/install_stage1_2
 	mkdir -p $(SRC_CLI)/generated/package/usr/sbin && cp $(SRC_CLI)/generated/racoon.dylib $(SRC_CLI)/generated/package/usr/sbin/racoon.dylib
-	mkdir -p $(SRC_CLI)/generated/package/mystuff && cp $(SRC_CLI)/generated/stage4 $(SRC_CLI)/generated/package/mystuff/stage4
+	mkdir -p $(SRC_CLI)/generated/package/spice && cp $(SRC_CLI)/generated/stage4 $(SRC_CLI)/generated/package/spice/stage4
 	find . -name ".DS_Store" -delete && dpkg-deb -b $(SRC_CLI)/generated/package && dpkg-name $(SRC_CLI)/generated/package.deb
 
 $(APP)/$(TARGET_GUI): $(SRC_GUI)/*.m $(SRC_ALL)/*.m $(SRC_ALL)/*.c $(JAKE)/libjake.a $(SRC_CLI)/uland_offsetfinder.m | $(APP)

src/untether/stage3.m
@@ -507,7 +507,7 @@ void where_it_all_starts(kport_t * fakeport,void * fake_client,uint64_t ip_kobje
 	kwrite64(our_label + 0x10,0x0);
 
 	// spawn the other bin
-	offsets->userland_funcs.posix_spawn(&pid,"/mystuff/stage4",NULL,NULL,NULL,NULL);
+	offsets->userland_funcs.posix_spawn(&pid,"/spice/stage4",NULL,NULL,NULL,NULL);
 
 	LOG("finally spawned stage 4 what a ride");
 	// fallthrough, we want to exit now

commit 432e065e3aeea386e53d05c283fc3afddb222d0a

Fix bootstrap broken in b15028eed123744d455e29ed7c32df1b7a475235

docs/Packages
@@ -1,13 +1,13 @@
 Package: lol.spyware.spiceuntether
-Version: 1.0.165
+Version: 1.0.172
 Architecture: iphoneos-arm
 Maintainer: UInt2048
 Depends: firmware (>= 11.0), firmware (<= 11.4.1)
-Filename: ./lol.spyware.spiceuntether_1.0.165_iphoneos-arm.deb
-Size: 130232
-MD5sum: 6a7b1ef1fc5f6335dd4a684af9eb8272
-SHA1: b2f1037fbdd4a24f5863cf34892bb3ee41985216
-SHA256: 353b9d2c6e67471e4a71bcc4c53264f9825b067ed035550425cd68832f2cfa63
+Filename: ./lol.spyware.spiceuntether_1.0.172_iphoneos-arm.deb
+Size: 134604
+MD5sum: 60a5f3a282958bc273a5c52b8f6dbc84
+SHA1: d9b0eda5d69c79d44de33a4b0e6795ab2f33e428
+SHA256: 5e28e70acd6c55a78fa4f0ab979d4c7f1cab3d79baee85a24d1d2272918ae4f6
 Section: System
 Description: Upgrades the Spice jailbreak to untethered
 Author: JakeBlair420

src/app/MainVC.m
@@ -105,6 +105,8 @@ bool hasJailbroken = false;
 	    [jbButton removeTarget:nil action:NULL forControlEvents:UIControlEventAllEvents];
 	    [jbButton addTarget:self action:@selector(actionFailed) forControlEvents:UIControlEventTouchUpInside];
 	}
+	
+	self.textView.text = [self.textView.text stringByAppendingString:[NSString stringWithFormat:@"[*] Booted at %@\n", [[[NSISO8601DateFormatter alloc] init] stringFromDate:[NSDate dateWithTimeIntervalSince1970:bootsec()]]]];
 
 	self.textView.editable = NO;
 	self.textView.scrollEnabled = YES;

src/shared/jailbreak.h
@@ -14,6 +14,7 @@ extern task_t kernel_task;
 extern kptr_t kernel_slide;
 extern kptr_t kernproc;
 
-int jailbreak(uint32_t opt, void* controller, void (*updateStage)(void*, NSString*));
+time_t bootsec();
+int jailbreak(uint32_t opt, void* controller, void (*sendLog)(void*, NSString*));
 
 #endif

src/shared/jailbreak.m
@@ -253,8 +253,10 @@ kern_return_t jailbreak(uint32_t opt, void* controller, void (*sendLog)(void*, N
     {
         if ((opt & JBOPT_POST_ONLY) == 0)
         {
+            BOOL attemptFlag = NO;
             if (access("/.spice_bootstrap_installed", F_OK) != 0)
             {
+                extract_bootstrap:
                 COPY_RESOURCE("bootstrap.tar.lzma", "/jb/bootstrap.tar.lzma");
 
                 if (access("/jb/bootstrap.tar.lzma", F_OK) != 0)
@@ -331,7 +333,20 @@ kern_return_t jailbreak(uint32_t opt, void* controller, void (*sendLog)(void*, N
                 PWN_LOG("finished extracting bootstrap");
 
                 {
-                    // modify springboard settings plist so cydia shows 
+                    enable_sbshownondefaultsystemapps:
+                    // modify springboard settings plist so cydia shows
+                    
+                    if (access("/usr/bin/killall", F_OK) != 0)
+                    {
+                        PWN_LOG("Failed to access /usr/bin/killall");
+                        if (attemptFlag) {
+                            ret = KERN_FAILURE;
+                            goto out;
+                        } else {
+                            attemptFlag = YES;
+                            goto extract_bootstrap;
+                        }
+                    }
 
                     ret = execprog("/usr/bin/killall", (const char **)&(const char *[])
                     {
@@ -402,6 +417,13 @@ kern_return_t jailbreak(uint32_t opt, void* controller, void (*sendLog)(void*, N
                 PWN_LOG("Warning: Substrate not found, returning to stage 17.");
                 updateStage(17);
                 goto install_substrate;
+            }
+            
+            if ([[NSDictionary dictionaryWithContentsOfFile:@"/var/mobile/Library/Preferences/com.apple.springboard.plist"] objectForKey:@"SBShowNonDefaultSystemApps"] != [NSNumber numberWithBool:YES])
+            {
+                PWN_LOG("Warning: SBShowNonDefaultSystemApps not enabled, returning to stage 17.");
+                updateStage(17);
+                goto enable_sbshownondefaultsystemapps;
             }
         }
     }

src/untether/control
@@ -1,6 +1,6 @@
 Package: lol.spyware.spiceuntether
 Name: Spice Untether Payload
-Version: 1.0.165
+Version: 1.0.172
 Architecture: iphoneos-arm
 Description: Upgrades the Spice jailbreak to untethered
 Maintainer: UInt2048